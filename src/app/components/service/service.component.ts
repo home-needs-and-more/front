e636232431974f343c89d782a8e5860f3fb2ee23
AliGarouachi <36104357+AliGarouachi@users.noreply.github.com>
2021-03-09 13:53:42 +0100

    Services cards

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
-
+  services = [{
+    name : "PAINTING",
+    price : "2,29",
+    image : "157522576_754810258793830_1658028758824288167_n.jpg",
+    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+  },
+  {
+    name : "PAINTING",
+    price : "2,29",
+    image : "157522576_754810258793830_1658028758824288167_n.jpg",
+    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+  }]
   constructor() { }
 
   ngOnInit(): void {
