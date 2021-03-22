3b9eb1c568f4dfc30cdd691c46b1226cb57eb9d8
AliGarouachi <36104357+AliGarouachi@users.noreply.github.com>
2021-03-22 12:56:56 +0100

    modal

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JobsService } from '../../services/jobs/jobs.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {JobModule} from '../../model/Job/job/job.module';
 @Component({
   selector: 'app-service',
@@ -7,6 +8,7 @@ import {JobModule} from '../../model/Job/job/job.module';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
+  closeResult = '';
   services = [{
     name : "PAINTING",
     price : "2,29",
@@ -19,13 +21,27 @@ export class ServiceComponent implements OnInit {
     image : "157522576_754810258793830_1658028758824288167_n.jpg",
     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   }]
-  constructor(private jobsService : JobsService) { }
+  constructor(private jobsService : JobsService,private modalService: NgbModal) { }
 
   ngOnInit(): void {
-    var job = new JobModule;
-    job.name = 'from front angular updated';
-    job.id = 6;
-    this.jobsService.update(job);
+    
   }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
 }
