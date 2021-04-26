b935301d44b7bc88575f66b180378634fa86e5b7
AliGarouachi <36104357+AliGarouachi@users.noreply.github.com>
2021-04-26 23:33:18 +0100

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://www.home-needs-and-more/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
